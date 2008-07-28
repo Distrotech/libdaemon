54a5c0546730efd7e8e54ac8cb06be0b7dfcea0d
Lennart Poettering <lennart@poettering.net>
2008-07-29 01:01:29 +0200

    Revert "bump soname/version" which was innappropriately named and a bad idea anyway
    This reverts commit b05a9ea7518befdae73772becaf9bc4b11711f80.

--- libdaemon/dexec.c
+++ libdaemon/dexec.c
@@ -182,7 +182,6 @@ int daemon_execv(const char *dir, int *ret, const char *prog, va_list ap) {
             if (sig != SIGCHLD) {
                 daemon_log(LOG_WARNING, "Killing child.");
                 kill(pid, SIGTERM);
-                break;
             }
         }
     }
